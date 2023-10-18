a9e953ceadbe04da8347b939ac23e5a258f5da24
Jacob Furman <jacobf1323@gmail.com>
2023-10-18 19:12:37 +0100

    added nav bar and work on link shortener

--- src/routes/r/[slug]/+page.svelte
+++ src/routes/r/[slug]/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <svelte:head>
-	<!--    <meta http-equiv="refresh" content="1;url={pageUrl}">-->
+	    <meta http-equiv="refresh" content="1;url={pageUrl}">
 	<!-- General Meta Tags -->
 	<meta name="title" content={title} />
 	<meta name="description" content={description} />
@@ -15,16 +15,25 @@
 	<!-- Open Graph -->
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
+	{#if imageUrl}
 	<meta property="og:image" content={imageUrl} />
+
+		{/if}
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={pageUrl} />
 
 	<!-- Twitter Cards -->
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={imageUrl} />
 
+	<meta name="twitter:site" content="@JaFu_py">
+	<meta name="twitter:creator" content="@JaFu_py">
+	<meta name="twitter:title" content="{title}">
+	<meta name="twitter:description" content="{description}">
+	{#if imageUrl}
+	<meta name="twitter:image" content="{imageUrl}">
+	<meta name="twitter:card" content="summary_large_image">
+		{:else }
+		<meta name="twitter:card" content="summary">
+		{/if}
 	<!-- Theme Color -->
-	<meta name="theme-color" content={colorCode} />
+	<meta name="theme-color" content={color} />
 </svelte:head>

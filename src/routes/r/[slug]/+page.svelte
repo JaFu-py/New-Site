20cb5d44d6dda7107c85544b3c2f46cb81253e12
jafu-py <jafu@JafBook-Air.home>
2023-12-20 22:07:35 +0000

    fixes

--- src/routes/r/[slug]/+page.svelte
+++ src/routes/r/[slug]/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-
-
-	export let data
-
-    const {title, description, imageUrl, pageUrl, color} = data;
+	export let data;
+	const { title, description, imageUrl, pageUrl, color } = data;
 </script>
 
 <svelte:head>
-	    <meta http-equiv="refresh" content="1;url={pageUrl}">
+	<meta http-equiv="refresh" content="1;url={pageUrl}" />
 	<!-- General Meta Tags -->
 	<meta name="title" content={title} />
 	<meta name="description" content={description} />
@@ -16,24 +13,23 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	{#if imageUrl}
-	<meta property="og:image" content={imageUrl} />
-
-		{/if}
+		<meta property="og:image" content={imageUrl} />
+	{/if}
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={pageUrl} />
 
 	<!-- Twitter Cards -->
 
-	<meta name="twitter:site" content="@JaFu_py">
-	<meta name="twitter:creator" content="@JaFu_py">
-	<meta name="twitter:title" content="{title}">
-	<meta name="twitter:description" content="{description}">
+	<meta name="twitter:site" content="@JaFu_py" />
+	<meta name="twitter:creator" content="@JaFu_py" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
 	{#if imageUrl}
-	<meta name="twitter:image" content="{imageUrl}">
-	<meta name="twitter:card" content="summary_large_image">
-		{:else }
-		<meta name="twitter:card" content="summary">
-		{/if}
+		<meta name="twitter:image" content={imageUrl} />
+		<meta name="twitter:card" content="summary_large_image" />
+	{:else}
+		<meta name="twitter:card" content="summary" />
+	{/if}
 	<!-- Theme Color -->
 	<meta name="theme-color" content={color} />
 </svelte:head>

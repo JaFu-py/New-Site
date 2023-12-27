58c441b51e087a0bfa92af611f177250504c4d7e
jafu-py <jacobf1323@gmail.com>
2023-12-27 18:21:21 +0000

    Fixes 2.0

--- src/routes/r/[slug]/+page.js
+++ src/routes/r/[slug]/+page.js
@@ -3,11 +3,7 @@ import supabase from '$lib/subabase.js';
 export async function load({ fetch, params }) {
 	const { data } = await supabase.from('links').select('*').eq('slug', params.slug);
 
-<<<<<<< Updated upstream
     console.log(JSON.stringify(data));
-=======
-	// console.log(JSON.stringify(data));
->>>>>>> Stashed changes
 
 	if (data.error) throw data.error;
 	return data
--- src/routes/r/[slug]/+page.svelte
+++ src/routes/r/[slug]/+page.svelte
@@ -1,15 +1,8 @@
 <script>
 	export let data;
-<<<<<<< HEAD
-	const { title, description, imageUrl, pageUrl, color } = data;
-=======
-
-<<<<<<< Updated upstream
-
-	export let data
 
     const {title, description, imageUrl, pageUrl, color} = data;
->>>>>>> d6e996ac182d9d0efce2deacf2e047aba8e7169b
+
 </script>
 
 <svelte:head>
@@ -42,45 +35,4 @@
 	<!-- Theme Color -->
 	<meta name="theme-color" content={color} />
 </svelte:head>
-=======
-	const { title, description, imageUrl, PageUrl, color } = data;
-</script>
 
-<svelte:head>
-	{#if data !== '404'}
-		<meta http-equiv="refresh" content="1;url={PageUrl}" />
-
-		<!-- General Meta Tags -->
-		<meta name="title" content={title} />
-		<meta name="description" content={description} />
-
-		<!-- Open Graph -->
-		<meta property="og:title" content={title} />
-		<meta property="og:description" content={description} />
-		{#if imageUrl}
-			<meta property="og:image" content={imageUrl} />
-		{/if}
-		<meta property="og:type" content="website" />
-		<meta property="og:url" content={PageUrl} />
-
-		<!-- Twitter Cards -->
-
-		<meta name="twitter:site" content="@JaFu_py" />
-		<meta name="twitter:creator" content="@JaFu_py" />
-		<meta name="twitter:title" content={title} />
-		<meta name="twitter:description" content={description} />
-		{#if imageUrl}
-			<meta name="twitter:image" content={imageUrl} />
-			<meta name="twitter:card" content="summary_large_image" />
-		{:else}
-			<meta name="twitter:card" content="summary" />
-		{/if}
-		<!-- Theme Color -->
-		<meta name="theme-color" content={color} />
-	{/if}
-</svelte:head>
-{#if data === '404'}
-	<h1>404</h1>
-	<p>This isn't a valid link, silly!</p>
-{/if}
->>>>>>> Stashed changes
